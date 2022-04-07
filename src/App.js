625c3fef28f6b3be208d5aa5fb82f1d5929a1936
markkravic <hbklksndr@protonmail.com>
2022-04-07 18:32:29 +0300

    Lesson_2
    useState, forms, components, index

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import {SingleUser, Users} from "./Components";
 
 const App = () => {
+    const [singleUser, setSingleUser] = useState(null);
+    const getUser = (user) => {
+      setSingleUser(user)
+    }
     return (
         <div>
-            App
+        <div>
+           {singleUser && <SingleUser user={singleUser}/>}
         </div>
+        <div>
+            <Users getUser={setSingleUser}/>
+        </div>
+            </div>
     );
 };
 
 export default App;
+
+
+// <Form2/>
+// <Form1/>
+// import css from "./App.module.css"
+
