5db822fd47ae44cd2e969370fa0d971281a0685b
markkravic <hbklksndr@protonmail.com>
2022-04-08 16:35:23 +0300

    hometask_2
    useState, forms, components, indexjs, axios usage, structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import styles from "./app.module.css"
+import {Users, UserInfo, Posts} from "./components";
 
 const App = () => {
+    const [user, setUser] = useState(null);
+    const [userIdForPosts, setUserIdForPosts] = useState(null);
     return (
         <div>
-            App
+            <div className={styles.usersAndInfo}>
+            <Users setUser={setUser} setUserIdForPosts={setUserIdForPosts}/>
+                {user && <UserInfo user={user} setUserIdForPosts={setUserIdForPosts} />}
+            </div>
+            {userIdForPosts && <Posts userId={userIdForPosts} />}
         </div>
     );
-};
+}
 
 export default App;
+
+
+
+// Вивести всіх юзерів, біля кожного юзера кнопка GetDetails.
+//     При натисканні на кнопку праворуч повинна з’явитися повністю вся інформація про юзера і кнопка при натисканні на яку знизу повинні з’явитися його пости
+// Структура схематично зображена вище...
---
+++ src/components/User/User.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const User = ({user, setUser, setUserIdForPosts}) => {
+    const click =() =>{
+        setUserIdForPosts(false);
+        setUser(user);
+    }
+    return (
+        <div>
+            {user.name}
+            <input type="button" value="show info" onClick={click}/>
+        </div>
+    );
+};
+
