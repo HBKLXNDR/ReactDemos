ee883203389c1585edc76a509866d50cb0eaef75
markkravic <hbklksndr@protonmail.com>
2022-05-04 18:01:34 +0300

    base_1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import {useDispatch, useSelector} from "react-redux";
 
 
 const App = () => {
+    let state = useSelector(state=> state);
+
+    let dispatch = useDispatch();
     return (
-        App
+        <div>
+            <h1>state is {state}</h1>
+            <button onClick={()=>{dispatch({type:"INC", payload: 1})}}>increment</button>
+            <button onClick={()=>{dispatch({type:"DEC", payload: "1"})}}>decrement</button>
+            <button onClick={()=>{dispatch({type:"RESET", payload: 0})}}>reset</button>
+        </div>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from "./App";
+import {Provider} from "react-redux";
+import {createStore} from "redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
+
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case "INC":
+            return state+ action.payload;
+        case "DEC":
+            return state- action.payload;
+
+        case "RESET":
+            state = action.payload
+            return state
+        default:
+            return state;
+    }
+
+}
+
+let store = createStore(reducer);
+
+
+root.render(<Provider store={store}>
+        <App/>
+    </Provider>
 )
