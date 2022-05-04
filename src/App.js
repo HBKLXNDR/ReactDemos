b755a1add3d013eb43fb411e2e4c62e1a001fb32
markkravic <hbklksndr@protonmail.com>
2022-05-04 16:00:00 +0300

    master back

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
 import React from 'react';
 
-import {Routes, Route} from "react-router-dom";
-import StartPage from "./components/StartPage/StartPage";
-import {First, HZpage} from "./components";
 
 const App = () => {
     return (
-        <Routes>
-            <Route path={"/"} element={<StartPage/>}>
-            </Route>
-            <Route path={"hz"} element={<HZpage/>}/>
-            <Route path={"about/:id"} element={<First/>}/>
-        </Routes>
+        App
     );
 };
 
