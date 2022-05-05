e1a0b8172ee959bcf2576a6eac463be373daec83
markkravic <hbklksndr@protonmail.com>
2022-05-05 17:07:47 +0300

    lesson7_base

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Counter} from "./components";
 
 
 const App = () => {
     return (
-        App
+        <div>
+            <Counter/>
+        </div>
     );
 };
 
