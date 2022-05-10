6da3340e997caa55d1d6ddbdc86e8f7de0fdb110
markkravic <hbklksndr@protonmail.com>
2022-05-11 00:11:49 +0300

    Rick_and_Morty_API

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {Routes,Route,Navigate} from "react-router-dom";
 
+import {MainLayout} from "./layouts";
+import {CharactersPage, EpisodesPage} from "./pages";
 
 const App = () => {
     return (
-        App
+        <Routes>
+            <Route path={"/"} element={<MainLayout/>}>
+                <Route index element={<Navigate to={"episodes"}/>}/>
+                <Route path={"episodes"} element={<EpisodesPage/>}/>\
+                <Route path={"characters"} element={<CharactersPage/>}/>
+            </Route>
+        </Routes>
     );
 };
 
-export default App;
+export {App};
