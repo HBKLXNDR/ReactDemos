3637db1ea2da2e30e35fc5aecea2f6f7c58da988
markkravic <hbklksndr@protonmail.com>
2022-05-30 12:27:10 +0300

    auth

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import {Navigate, Route, Routes} from "react-router-dom";
 
+import {MainLayout} from "./layouts";
+import {CarPage, LoginPage, RegisterPage} from "./pages";
 
 const App = () => {
     return (
-        App
+        <Routes>
+            <Route path={"/"} element={<MainLayout/>}>
+                <Route index element={<Navigate to={"cars"}/>}/>
+                <Route path={"cars"} element={<CarPage/>}/>
+                <Route path={"login"} element={<LoginPage/>}/>
+                <Route path={"register"} element={<RegisterPage/>}/>
+
+            </Route>
+        </Routes>
     );
 };
 
